2e58de472bd79bdec5ec9d4d6211423b35d08b05
yangzhong666 <53607375+yangzhong666@users.noreply.github.com>
2019-08-09 09:30:04 +0800

    圆点坐标文件

---
+++ Circle.java
@@ -0,0 +1,20 @@
+package com.neuedu.test;
+import java.awt.Point;
+
+
+public class Circle{
+	public  int x;
+	public  int y;
+	public Circle(int i,int b){
+		this.x = i ;
+		this.x = b ;
+		
+	}
+		public static void main(String[] args) {
+			Point point = new Point(2,1);
+			System.out.println("点的坐标为:"+"("+point.x+","+point.y+")");
+		}
+		
+		
+		
+}
